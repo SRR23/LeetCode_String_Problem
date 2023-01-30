7dd969b9e9c3b12019693134e9bf140edb986ea6
SRR23 <srr267@gmail.com>
2023-01-30 22:50:57 +0600

    added

---
+++ FirstUniqueCharacter_387.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstUniqChar(string s)
+{
+    map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
+    }
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp[s[i]] == 1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s = "loveleetcode";
+    cout << firstUniqChar(s) << '\n';
+}
